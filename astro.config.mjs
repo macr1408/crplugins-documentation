b620189aa0e496920092899ca2d39de6ee1c99b4
Marcos Carpio <macr1408@hotmail.com>
2024-07-09 14:29:21 -0300

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,27 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'CRPlugins',
+      credits: true,
+      tableOfContents: true,
+      locales: {
+        root: {
+          label: 'Español',
+          lang: 'es',
+        },
+      },
+      sidebar: [
+        {
+          label: 'Introducción',
+          slug: 'index',
+        },
+        {
+          label: 'OCA Para WooCommerce',
+          autogenerate: { directory: 'oca' },
+        },
+      ],
+    }),
+  ],
 });
---
+++ src/content/docs/oca/instalacion.md
@@ -0,0 +1,21 @@
+---
+title: Instalación
+---
+
+Con este plugin podrás conectar tu tienda con los servicios de OCA.
+
+Podrás cotizar, procesar pedidos, imprimir etiquetas, rastrear los pedidos, cancelar pedidos, y mucho más.
+
+## Requisitos
+
+- Cuenta activa de OCA con listado de operativas
+- PHP 7.1 o mayor
+- WooCommerce 4 o mayor
+
+## Instalación
+
+- Accedé al Panel de Administración
+- En el menú de la izquierda, seleccioná **Plugins** y luego **Añadir nuevo**
+- En la barra de búsqueda en la parte superior derecha, escribe el nombre OCA para WooCommerce.
+- Instalá nuestro plugin
+- Activá el plugin
