f849797110408a4b3ae471f55ae5b2a4536969e3
Marcos Carpio <macr1408@hotmail.com>
2024-07-09 14:37:49 -0300

    remove ssr support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
   integrations: [
     starlight({
       title: 'CRPlugins',
