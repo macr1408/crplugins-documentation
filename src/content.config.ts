ab1ec96b682c5a1e53ee66d2a6ee782971040eda
Marcos Carpio <macr1408@hotmail.com>
2025-02-28 10:53:10 -0300

    Updated instructions and version to astro v6

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection } from 'astro:content';
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
+import { i18nLoader } from '@astrojs/starlight/loaders';
 
 export const collections = {
   docs: defineCollection({ schema: docsSchema() }),
-  i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
+  i18n: defineCollection({ loader: i18nLoader(), schema: i18nSchema() }),
 };
